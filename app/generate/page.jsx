b46ba01f7d50818f8cbf07c1a9982446b5f1d468
Oaksoe11 <osk@sfu.ca>
2025-05-31 01:35:17 -0700

    Add GeneratePage with Flashcardform component

---
+++ app/generate/page.jsx
@@ -0,0 +1,26 @@
+'use client'
+
+import React from 'react'
+import Flashcardform from '../components/Flashcardform'
+
+const GeneratePage = () => {
+  return (
+    <>
+      <div className="min-h-screen bg-[#11001f] text-white pt-32 px-6 flex flex-col items-center">
+        <h1 className="text-4xl font-extrabold text-center mb-10">
+          Generate Your{' '}
+          <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-cyan-400">
+            Flashcards
+          </span>
+        </h1>
+
+        {/* This div centers the Flashcard horizontally */}
+        <div className="w-full max-w-2xl">
+          <Flashcardform />
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default GeneratePage
